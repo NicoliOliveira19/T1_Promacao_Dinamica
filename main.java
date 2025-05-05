029f46d1718b5eb292f7b8becf7699e339dbeced
Nicoli Siqueira <nicoli.siqueira@sap.com>
2025-05-05 18:52:12 -0300

    Código finalizado (agora sim)

--- main.java
+++ main.java
@@ -1,2 +1,10 @@
 public class main {
+    LongestCommonSubsequence mc = new LongestCommonSubsequence();
+
+    String s1 = "abcdefg";
+    String s2 = "acfg";
+
+    int resultado = mc.maiorComum(s1, s2);
+
+    //esperado (a,c,f,g) tamanho 4
 }
